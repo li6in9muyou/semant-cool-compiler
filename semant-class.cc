9bd4b045eab32226f9f56935b3d2aa24b6ba3fb3
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-04-09 18:13:39 +0800

    implment checks on superclasses

--- semant-class.cc
+++ semant-class.cc
@@ -1,8 +1,12 @@
 #include <symtab.h>
 #include <string>
+#include <algorithm>
+#include <array>
 using std::string;
 
 #include "semant.h"
+#include "cool-tree.h"
+
 string error_message_superclass_is_in_cycle(const string &class_name);
 string error_message_superclass_is_not_defined(const string &class_name, const string &parent_name);
 string error_message_superclass_is_primitive(const string &class_name, const string &parent_name);
@@ -45,6 +49,14 @@ void class__class::check_not_redefined_and_register(SemantContext &ctx)
 
 void class__class::check_superclass_is_defined(SemantContext &ctx)
 {
+    const auto bad = ctx.classTable.probe(parent) == nullptr;
+    if (bad)
+    {
+        ctx.semant_error(this) << error_message_superclass_is_not_defined(
+                                      name->get_string(),
+                                      parent->get_string())
+                               << "\n";
+    }
 }
 
 void class__class::check_superclass_is_not_in_cycle(SemantContext &ctx)
@@ -53,6 +65,28 @@ void class__class::check_superclass_is_not_in_cycle(SemantContext &ctx)
 
 void class__class::check_superclass_is_not_primitives(SemantContext &ctx)
 {
+    auto bad = false;
+
+    if (Bool->equal_string(parent->get_string(), parent->get_len()))
+    {
+        bad = true;
+    }
+    if (Int->equal_string(parent->get_string(), parent->get_len()))
+    {
+        bad = true;
+    }
+    if (Str->equal_string(parent->get_string(), parent->get_len()))
+    {
+        bad = true;
+    }
+
+    if (bad)
+    {
+        ctx.semant_error(this) << error_message_superclass_is_primitive(
+                                      name->get_string(),
+                                      parent->get_string())
+                               << "\n";
+    }
 }
 
 void class__class::check_Main_has_main(SemantContext &ctx)
