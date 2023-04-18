94d279de8a30acf5ea306d7531b50213261ba67c
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-04-18 22:04:55 +0800

    check symobl with probe semantics

--- semant-checks.h
+++ semant-checks.h
@@ -24,3 +24,36 @@ bool check_symbol_exists(const Symbol &want, SymbolTable<Symbol, T> &scope, func
     }
     return ok;
 }
+
+template <class T>
+bool check_symbol_not_exists(const Symbol &forbid, SymbolTable<Symbol, T> &scope, function<void()> on_error = noop)
+{
+    const auto ok = scope.lookup(forbid) == nullptr;
+    if (!ok)
+    {
+        on_error();
+    }
+    return ok;
+}
+
+template <class T>
+bool check_symbol_exists_in_current_scope(const Symbol &want, SymbolTable<Symbol, T> &scope, function<void()> on_error = noop)
+{
+    const auto ok = scope.probe(want) != nullptr;
+    if (!ok)
+    {
+        on_error();
+    }
+    return ok;
+}
+
+template <class T>
+bool check_symbol_not_exists_in_current_scope(const Symbol &forbid, SymbolTable<Symbol, T> &scope, function<void()> on_error = noop)
+{
+    const auto ok = scope.probe(forbid) == nullptr;
+    if (!ok)
+    {
+        on_error();
+    }
+    return ok;
+}
