0b9d23002fcdf65e794befd53b3174a5dae49338
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-04-29 23:01:24 +0800

    enforce prerequisite on assign type conform checks

--- semant-assign.cc
+++ semant-assign.cc
@@ -17,25 +17,42 @@ bool assign_class::semant(SemantContext &ctx)
                       "Cannot assign to 'self'.\n");
         });
 
-    ok &= expr->semant(ctx);
+    const auto exprOk = expr->semant(ctx);
+    ok &= exprOk;
     if (check_symbol_eq(No_type, expr->get_type()))
     {
         LOG_F(INFO, "rhs is a No_type, skip type conform check");
         return ok;
     }
+    const auto exprType = expr->get_type();
+    const auto exprTypeExists = check_symbol_exists(exprType, ctx.classTable);
+    LOG_IF_F(INFO, !exprTypeExists, "expr type %s does not exist, skip type conform check", exprType->get_string());
+    set_type_if_ok(exprTypeExists, this, expr->get_type(), Object);
 
-    const auto type_decl = *ctx.typeEnv->lookup(name);
-    ok &= set_type_if_ok(
-        check_type_conform_to(
-            ctx, expr->get_type(), type_decl,
-            [&]()
-            {
-                err.print(location(ctx.filename, get_line_number()) +
-                          "Type " + expr->get_type()->get_string() +
-                          " of assigned expression does not conform to declared type " + type_decl->get_string() +
-                          " of identifier " + name->get_string() + ".\n");
-            }),
-        this, type_decl, Object);
-
+    const auto idExists = check_symbol_exists(
+        name, *ctx.typeEnv,
+        [&]()
+        {
+            err.print(location(ctx.filename, get_line_number()) +
+                      "Assignment to undeclared variable " +
+                      name->get_string() + ".\n");
+        });
+    if (idExists)
+    {
+        const auto idType = *ctx.typeEnv->lookup(name);
+        const auto idTypeExists = check_symbol_exists(idType, ctx.classTable);
+        LOG_IF_F(INFO, !idTypeExists, "id type %s does not exist, skip type conform check", idType->get_string());
+        ok &= idTypeExists && idExists &&
+              check_type_conform_to(
+                  ctx, exprType, idType,
+                  [&]()
+                  {
+                      err.print(location(ctx.filename, get_line_number()) +
+                                "Type " + expr->get_type()->get_string() +
+                                " of assigned expression does not conform to declared type " +
+                                idType->get_string() +
+                                " of identifier " + name->get_string() + ".\n");
+                  });
+    }
     return ok;
 }
