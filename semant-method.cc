aa595e6354fa9252e2acd3e0ea5c4abd217a1698
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-04-29 22:58:01 +0800

    check type conform only if return type exists

--- semant-method.cc
+++ semant-method.cc
@@ -12,15 +12,6 @@ bool method_class::semant(SemantContext &ctx)
     LOG_F(INFO, "semant at method %s", name->get_string());
     auto ok = true;
 
-    ok &= check_symbol_exists(
-        return_type,
-        ctx.classTable,
-        [&]()
-        {
-            err.print(location(ctx.filename, get_line_number()) +
-                      "Undefined return type " + return_type->get_string() + " in method " + name->get_string() + ".\n");
-        });
-
     LOG_F(INFO, "enter scope on %p", ctx.typeEnv);
     ctx.typeEnv->enterscope();
 
@@ -33,7 +24,16 @@ bool method_class::semant(SemantContext &ctx)
 
     LOG_F(INFO, "descend into expression");
     ok = expr->semant(ctx) && ok;
-    ok = ok &&
+
+    const auto returnTypeOk = check_symbol_exists(
+        return_type, ctx.classTable,
+        [&]()
+        {
+            err.print(location(ctx.filename, get_line_number()) +
+                      "Undefined return type " + return_type->get_string() + " in method " + name->get_string() + ".\n");
+        });
+    LOG_IF_F(INFO, !returnTypeOk, "return type %s does no exist", return_type->get_string());
+    ok = returnTypeOk &&
          check_type_conform_to(
              ctx, expr->get_type(), return_type,
              [&]()
@@ -43,7 +43,8 @@ bool method_class::semant(SemantContext &ctx)
                            " of method " + name->get_string() +
                            " does not conform to declared return type " +
                            return_type->get_string() + ".\n");
-             });
+             }) &&
+         ok;
     ctx.typeEnv->exitscope();
     return ok;
 }
