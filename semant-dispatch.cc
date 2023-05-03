d2e8c8fba42de2ccb3502d6e8af8d796037a00a2
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-05-03 15:56:53 +0800

    passes static dispatch bad tests

--- semant-dispatch.cc
+++ semant-dispatch.cc
@@ -1,3 +1,5 @@
+#include <functional>
+using std::function;
 #include <iterator>
 using std::prev;
 
@@ -5,6 +7,53 @@ using std::prev;
 #include "semant-utility.h"
 #include "semant-checks.h"
 #include "loguru.h"
+
+bool check_actual_args(
+    SemantContext &ctx, const std::vector<std::pair<Symbol, Symbol>> &impl, Expressions actual,
+    function<void()> on_bad_arg_len,
+    function<void(const Symbol &formalName, const Symbol &formalType, const Symbol &actualType)> on_bad_type)
+{
+    LOG_F(INFO, "at check actual args");
+    auto ok = true;
+    {
+        LOG_SCOPE_F(INFO, "descend into actual arguments of length %d", actual->len());
+        for (auto i = actual->first(); actual->more(i); i = actual->next(i))
+        {
+            ok = ((Expression)actual->nth(i))->semant(ctx) && ok;
+        }
+    }
+
+    LOG_F(INFO, "check actual args and formal args have same length");
+    const auto implArgLen = ((int)impl.size()) - 1;
+    const auto argLenOk = implArgLen == actual->len();
+    if (!argLenOk)
+    {
+        LOG_F(INFO, "%d actual args != %d formal args", actual->len(), implArgLen);
+        on_bad_arg_len();
+    }
+    ok = argLenOk && ok;
+    if (argLenOk && actual->len() > 0)
+    {
+        LOG_SCOPE_F(INFO, "check actual args type conform to formals");
+        auto actualIt = actual->first();
+        for (auto i = 0; i < (int)impl.size() - 1; i++)
+        {
+            const auto p = impl.at(i);
+
+            const auto formalName = p.first;
+            const auto formalType = p.second;
+            const auto actualType = actual->nth(actualIt)->get_type();
+            actualIt = actual->next(actualIt);
+            ok &= check_type_conform_to(
+                ctx, actualType, formalType,
+                [&]()
+                {
+                    on_bad_type(formalName, formalType, actualType);
+                });
+        }
+    }
+    return ok;
+}
 
 bool dispatch_class::semant(SemantContext &ctx)
 {
@@ -66,5 +115,62 @@ bool dispatch_class::semant(SemantContext &ctx)
 bool static_dispatch_class::semant(SemantContext &ctx)
 {
     LOG_F(INFO, "semant at static dispatch at line %d", get_line_number());
-    return true;
+    auto ok = true;
+    ok &= check_symbol_not_eq(
+        type_name, SELF_TYPE,
+        [&]()
+        {
+            err.print(LOC + "Static dispatch to SELF_TYPE.\n");
+        });
+    ok &= check_symbol_exists(
+        type_name, ctx.classTable,
+        [&]()
+        {
+            err.print(LOC + "Static dispatch to undefined class " + type_name->get_string() + ".\n");
+        });
+    if (ok)
+    {
+        ok &= expr->semant(ctx);
+        ok &= check_type_conform_to(
+            ctx, expr->get_type(), type_name,
+            [&]()
+            {
+                err.print(LOC + "Expression type " +
+                          expr->get_type()->get_string() + " does not conform to declared static dispatch type " +
+                          type_name->get_string() + ".\n");
+            });
+        auto &receiverFamilyFeatures = ctx.programFeatureTable.at(type_name).methods;
+        ok &= check_symbol_exists(
+            name, receiverFamilyFeatures,
+            [&]()
+            {
+                err.print(LOC + "Static dispatch to undefined method " + name->get_string() + ".\n");
+            });
+        if (ok)
+        {
+            const auto impl = receiverFamilyFeatures.lookup(name);
+            // TODO: check actual argument types
+            ok &= check_actual_args(
+                ctx, *impl, actual,
+                [&]()
+                {
+                    err.print(LOC + "Method " + name->get_string() + " invoked with wrong number of arguments.\n");
+                },
+                [&](const Symbol &formalName, const Symbol &formalType, const Symbol &actualType)
+                {
+                    err.print(LOC +
+                              "In call of method " +
+                              name->get_string() + ", type " +
+                              actualType->get_string() + " of parameter " +
+                              formalName->get_string() + " does not conform to declared type " +
+                              formalType->get_string() + ".\n");
+                });
+            set_type(impl->back().second);
+        }
+        else
+        {
+            set_type(Object);
+        }
+    }
+    return ok;
 }
