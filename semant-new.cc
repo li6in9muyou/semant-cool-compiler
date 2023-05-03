dacced649e434082e76d6d34c478aa3b8e45c26c
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-05-03 17:18:22 +0800

    refactor: move lambdas out of biz logic

--- semant-new.cc
+++ semant-new.cc
@@ -8,19 +8,17 @@ using std::string;
 
 bool new__class::semant(SemantContext &ctx)
 {
+    const auto &undefined_type = [&]()
+    {
+        err.print(location(ctx.filename, get_line_number()) +
+                  "'new' used with undefined class " + type_name->get_string() + ".\n");
+    };
+
     LOG_F(INFO, "semant at 'new %s'", type_name->get_string());
     auto ok = true;
 
     ok &= check_symbol_eq(type_name, SELF_TYPE) ||
-          check_symbol_exists(
-              type_name,
-              ctx.classTable,
-              [&]()
-              {
-                  err.print(location(ctx.filename, get_line_number()) +
-                            "'new' used with undefined class " + type_name->get_string() + ".\n");
-              });
-
+          check_symbol_exists(type_name, ctx.classTable, undefined_type);
     set_type_if_ok(ok, this, type_name, Object);
     return ok;
 }
