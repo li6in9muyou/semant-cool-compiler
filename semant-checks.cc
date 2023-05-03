e0101738a15d87a35bee1a667ba447c0bd2ea366
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-05-03 17:15:59 +0800

    Revert "refactor: remove unused functions"
    This reverts commit 0db07c19480cb838938d543df54e51700a323959.

--- semant-checks.cc
+++ semant-checks.cc
@@ -3,6 +3,24 @@
 #include "semant-utility.h"
 #include "semant-checks.h"
 
+bool define_symbol_if_not_defined_already(
+    SymbolTable<Symbol, tree_node> &scope,
+    const Symbol &symbol,
+    tree_node *node,
+    function<void()> on_error)
+{
+    const auto ok = scope.lookup(symbol) == nullptr;
+    if (!ok)
+    {
+        on_error();
+    }
+    else
+    {
+        scope.addid(symbol, node);
+    }
+    return ok;
+}
+
 bool check_symbol_not_eq(const Symbol &thiz, const Symbol &that, function<void()> on_error)
 {
     const auto ok = !check_symbol_eq(thiz, that);
@@ -31,6 +49,19 @@ void noop()
 {
 }
 
+bool check_operands_are_integer_after_semant(SemantContext &ctx, Expression &e1, Expression &e2, function<void()> on_error)
+{
+    auto ok = true;
+
+    ok &= e1->semant(ctx);
+    ok &= e2->semant(ctx);
+
+    const auto e1Type = e1->get_type();
+    const auto e2Type = e2->get_type();
+    ok &= (check_symbol_eq(Int, e1Type, on_error) && check_symbol_eq(Int, e2Type, on_error));
+
+    return ok;
+}
 
 bool check_type_conform_to(SemantContext &ctx, const Symbol &t, const Symbol &super, function<void()> on_error)
 {
