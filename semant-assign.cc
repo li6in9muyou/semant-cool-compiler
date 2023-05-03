155a882598e22a9a9047d2195bed105ecd6e7be0
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-05-03 16:51:39 +0800

    refactor: move lambdas out of biz logic

--- semant-assign.cc
+++ semant-assign.cc
@@ -5,17 +5,31 @@
 
 bool assign_class::semant(SemantContext &ctx)
 {
+    const auto &cant_assign_self = [&]()
+    {
+        err.print(LOC + "Cannot assign to 'self'.\n");
+    };
+    const auto &var_undefined = [&]()
+    {
+        err.print(LOC + "Assignment to undeclared variable " +
+                  name->get_string() + ".\n");
+    };
+    const auto &bad_type = [&](const auto &idType)
+    {
+        return [&]()
+        {
+            err.print(LOC + "Type " +
+                      expr->get_type()->get_string() + " of assigned expression does not conform to declared type " +
+                      idType->get_string() + " of identifier " +
+                      name->get_string() + ".\n");
+        };
+    };
+
     LOG_F(INFO, "semant at '<-' at line %d", get_line_number());
 
     auto ok = true;
 
-    ok &= check_symbol_not_eq(
-        name, self,
-        [&]()
-        {
-            err.print(location(ctx.filename, get_line_number()) +
-                      "Cannot assign to 'self'.\n");
-        });
+    ok &= check_symbol_not_eq(name, self, cant_assign_self);
 
     const auto exprOk = expr->semant(ctx);
     ok &= exprOk;
@@ -29,30 +43,13 @@ bool assign_class::semant(SemantContext &ctx)
     LOG_IF_F(INFO, !exprTypeExists, "expr type %s does not exist, skip type conform check", exprType->get_string());
     set_type_if_ok(exprTypeExists, this, expr->get_type(), Object);
 
-    const auto idExists = check_symbol_exists(
-        name, *ctx.typeEnv,
-        [&]()
-        {
-            err.print(location(ctx.filename, get_line_number()) +
-                      "Assignment to undeclared variable " +
-                      name->get_string() + ".\n");
-        });
+    const auto idExists = check_symbol_exists(name, *ctx.typeEnv, var_undefined);
     if (idExists)
     {
         const auto idType = *ctx.typeEnv->lookup(name);
         const auto idTypeExists = check_symbol_exists(idType, ctx.classTable);
         LOG_IF_F(INFO, !idTypeExists, "id type %s does not exist, skip type conform check", idType->get_string());
-        ok &= idTypeExists && idExists &&
-              check_type_conform_to(
-                  ctx, exprType, idType,
-                  [&]()
-                  {
-                      err.print(location(ctx.filename, get_line_number()) +
-                                "Type " + expr->get_type()->get_string() +
-                                " of assigned expression does not conform to declared type " +
-                                idType->get_string() +
-                                " of identifier " + name->get_string() + ".\n");
-                  });
+        ok &= idTypeExists && idExists && check_type_conform_to(ctx, exprType, idType, bad_type(idType));
     }
     return ok;
 }
