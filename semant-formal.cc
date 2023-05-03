2985f9531fdbdfdd9207382577edc70b1242c5e1
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-05-03 17:13:32 +0800

    refactor: move lambdas out of biz logic

--- semant-formal.cc
+++ semant-formal.cc
@@ -10,38 +10,35 @@ using std::pair;
 
 bool formal_class::semant(SemantContext &ctx)
 {
+    const auto &bad_type = [&]()
+    {
+        err.print(location(ctx.filename, get_line_number()) +
+                  "Class " + type_decl->get_string() + " of formal parameter " + name->get_string() + " is undefined.\n");
+    };
+    const auto &no_SELF_TYPE = [&]()
+    {
+        err.print(location(ctx.filename, get_line_number()) +
+                  "Formal parameter " + name->get_string() + " cannot have type SELF_TYPE.\n");
+    };
+
     LOG_F(INFO, "semant at formal %s", name->get_string());
     auto ok = true;
 
-    ok &= check_symbol_exists(
-        type_decl,
-        ctx.classTable,
-        [&]()
-        {
-            err.print(location(ctx.filename, get_line_number()) +
-                      "Class " + type_decl->get_string() + " of formal parameter " + name->get_string() + " is undefined.\n");
-        });
-    ok &= check_symbol_not_eq(
-        type_decl,
-        SELF_TYPE,
-        [&]()
-        {
-            err.print(location(ctx.filename, get_line_number()) +
-                      "Formal parameter " + name->get_string() + " cannot have type SELF_TYPE.\n");
-        });
+    ok &= check_symbol_exists(type_decl, ctx.classTable, bad_type);
+    ok &= check_symbol_not_eq(type_decl, SELF_TYPE, no_SELF_TYPE);
 
     return ok;
 }
 
 bool formal_class::register_symbol(SemantContext &ctx)
 {
+    const auto &duplicate = [&]()
+    {
+        err.print(LOC + "Formal parameter " + name->get_string() + " is multiply defined.\n");
+    };
+
     LOG_F(INFO, "register symbol at formal %s using scope %p", name->get_string(), ctx.typeEnv);
-    const auto ok = check_symbol_not_exists_in_current_scope(
-        name, *ctx.typeEnv,
-        [&]()
-        {
-            err.print(LOC + "Formal parameter " + name->get_string() + " is multiply defined.\n");
-        });
+    const auto ok = check_symbol_not_exists_in_current_scope(name, *ctx.typeEnv, duplicate);
     if (ok)
     {
         ctx.typeEnv->addid(name, &type_decl);
